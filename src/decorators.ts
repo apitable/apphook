dec2bc6ada790ac865bd3ba44e9a406075bc2ff3
Kelly Peilin Chan <kelly@vikadata.com>
2022-11-09 10:07:09 +0800

    chore: decorator desc

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,10 +2,19 @@ import { AppHook } from "./apphook";
 import { TriggerCommand } from "./commands";
 import { IRule } from "./rules";
 
+/**
+ * Decorator for apphook.addTrigger
+ * 
+ * @param apphook 
+ * @param hook 
+ * @param args 
+ * @param rule 
+ * @param priority 
+ * @param isCatch 
+ * @returns 
+ */
 export function AddTrigger(apphook: AppHook, hook: string, args: any[] = [], rule: IRule | undefined = undefined, priority?: number, isCatch?: boolean ): MethodDecorator {
-
     return (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
-
         const actionCommand = target[propertyKey] as TriggerCommand;
         apphook.addTrigger(
             hook,
