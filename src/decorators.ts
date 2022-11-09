5180174088a6a42c3083567e5684b960383ad2a6
Kelly Peilin Chan <kelly@vikadata.com>
2022-11-09 10:15:06 +0800

    chore: prefix useless var

--- src/decorators.ts
+++ src/decorators.ts
@@ -14,7 +14,7 @@ import { IRule } from "./rules";
  * @returns 
  */
 export function AddTrigger(apphook: AppHook, hook: string, args: any[] = [], rule: IRule | undefined = undefined, priority?: number, isCatch?: boolean ): MethodDecorator {
-    return (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+    return (target: any, propertyKey: string | symbol, _descriptor: PropertyDescriptor) => {
         const actionCommand = target[propertyKey] as TriggerCommand;
         apphook.addTrigger(
             hook,
